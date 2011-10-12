893ed9d58caa902926ce6f1ec61ecb9a80109e80
Nathanael Jones <nathanael.jones@gmail.com>
2011-10-12 15:28:25 +0200

    First draft of plugin behavior.

--- lib/nesta-plugin-tags/init.rb
+++ lib/nesta-plugin-tags/init.rb
@@ -10,4 +10,18 @@ module Nesta
   class App
     helpers Nesta::Plugin::Tags::Helpers
   end
+  
+  class Page    
+    def articles_by_tags
+       Page.find_articles.select { |article| not (article.tags & self.tags).empty? }
+     end
+     def self.articles_by_tag(tag)
+        Page.find_articles.select { |article| not (article.tags & [tag]).empty? }
+      end
+
+     def tags
+       strings = metadata('tags')
+       strings.nil? ? [] : strings.split(',').map { |string| string.strip }
+     end
+  end
 end
